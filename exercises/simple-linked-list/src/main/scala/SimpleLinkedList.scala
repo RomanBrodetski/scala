72fd381b82ffe48d3043648083980a2327b2dc14
Roman Brodetski <roman.brodetski@gmail.com>
2018-01-10 10:56:43 +0100

    simple linked list

--- exercises/simple-linked-list/src/main/scala/SimpleLinkedList.scala
+++ exercises/simple-linked-list/src/main/scala/SimpleLinkedList.scala
@@ -1,8 +1,38 @@
-trait SimpleLinkedList[T] {
+trait SimpleLinkedList[+T] {
   def isEmpty: Boolean
   def value: T
-  def add(item: T): SimpleLinkedList[T]
+  def add[Q >: T](item: Q): SimpleLinkedList[Q]
   def next: SimpleLinkedList[T]
-  def reverse: SimpleLinkedList[T]
   def toSeq: Seq[T]
+  def reverse: SimpleLinkedList[T] = {
+    @annotation.tailrec
+    def reverseRec(list: SimpleLinkedList[T], acc: SimpleLinkedList[T]): SimpleLinkedList[T] = {
+      list match {
+        case Null => acc
+        case Cons(v, next)  => reverseRec(next, Cons(v, acc))
+      }
+    }
+    reverseRec(this, Null)
+  }
+}
+
+object SimpleLinkedList {
+  def apply[T](): SimpleLinkedList[T] = Null
+  def apply[T](ts: T*): SimpleLinkedList[T] = fromSeq(ts)
+  def fromSeq[T](seq: Seq[T]): SimpleLinkedList[T] =
+    seq.foldLeft(SimpleLinkedList[T]())((acc, t) => acc.add(t))
+}
+
+case object Null extends SimpleLinkedList[Nothing] {
+  override val isEmpty = true
+  override def value = throw new NoSuchElementException()
+  override def next = throw new NoSuchElementException()
+  override def add[Q](item: Q): Cons[Q] = Cons(item, Null)
+  override def toSeq = Nil
+}
+
+case class Cons[T](override val value: T, override val next: SimpleLinkedList[T]) extends SimpleLinkedList[T] {
+  override val isEmpty = false
+  override def add[Q >: T](item: Q): Cons[Q] = Cons(value, next.add(item))
+  override val toSeq = value +: next.toSeq
 }
